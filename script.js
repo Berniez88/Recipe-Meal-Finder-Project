917d4d8a7634baf09260863f67eb00838d6a107a
Bernie Zhong <berniezhong@gmail.com>
2022-03-22 16:32:25 -0400

    Removed any additional comments/duplicate code

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const recipeCloseBtn = document.getElementById("recipe-close-btn");
 //Event Listeners
 searchBtn.addEventListener("click", getMealList);
 mealList.addEventListener("click", getMealRecipe);
-// recipeCloseBtn.addEventListener("click", () => {
-//   mealDetailsContent.parentElement.classList.remove("showRecipe");
-// });
+
 recipeCloseBtn.addEventListener("click", () => {
   mealDetailsContent.parentElement.classList.remove("showRecipe");
 });
@@ -16,13 +14,11 @@ recipeCloseBtn.addEventListener("click", () => {
 //Getting meal list that matches with the ingredients
 function getMealList() {
   let searchInputTxt = document.getElementById("search-input").value.trim();
-  // Instead of https://www.themealdb.com/api/json/v1/1/filter.php?i=egg we can do ${} to search what the user inputs
   fetch(
     `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
   )
     .then((response) => response.json())
     .then((data) => {
-      // data will display an array of meals the user searches
       let html = "";
       if (data.meals) {
         data.meals.forEach((meal) => {
